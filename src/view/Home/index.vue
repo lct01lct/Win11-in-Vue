3b7ed9ebbfffcd115a00976e6148215270d247db
SG <2767525216@qq.com>
2022-08-05 10:06:06 +0800

    fix: change taskbar's way

--- src/view/Home/index.vue
+++ src/view/Home/index.vue
@@ -1,33 +1,18 @@
 <template>
-<<<<<<< HEAD
-  <div class="root">
-    <settingsVue></settingsVue>
-    <taskBarVue></taskBarVue>
-=======
   <div>
     <settingsVue></settingsVue>
-    <Folder></Folder>
->>>>>>> account
+    <taskBarVue></taskBarVue>
+    <!-- <Folder></Folder> -->
   </div>
 </template>
 
 <script setup>
-  import settingsVue from '../../component/setting/settings.vue';
-  import taskBarVue from '../../component/taskBar/taskBar.vue';
+  import settingsVue from '@/component/setting/index.vue';
+  import taskBarVue from '@/component/taskBar/taskBar.vue';
 </script>
 
 <style lang="scss" scoped>
-  .root {
-    background-image: url(@/assets/img/mainPage/win10.png);
-
-    min-width: 100vw;
-    min-height: 100vh;
-
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-  }
-  * {
-    box-sizing: border-box;
-  }
+  // * {
+  //   box-sizing: border-box;
+  // }
 </style>
