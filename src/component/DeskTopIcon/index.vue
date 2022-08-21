6f95ce9bee9ee174240c91380e8070795e959445
SG <2767525216@qq.com>
2022-08-21 18:30:47 +0800

    路径

--- src/component/DeskTopIcon/index.vue
+++ src/component/DeskTopIcon/index.vue
@@ -48,7 +48,7 @@
     const X = e.pageX;
     const Y = e.pageY;
     // 清除所有已选中的样式
-    // Array.from(IconsRef.children).map((value) => value.classList.remove('selected'));
+    Array.from(IconsRef.children).map((value) => value.classList.remove('selected'));
     const move = (e) => {
       // 改变模态框的大小
       const tempX = e.pageX < X ? e.pageX : X;
@@ -58,9 +58,9 @@
 
       setWidth(e.pageX - X, e.pageY - Y, ModalFrameRef);
       // 当前被选中的元素
-      const inContains = judgeContains(IconsRef, ModalFrameRef);
-      // 存储状态
-      deskTopStore.changeCurrentSelected(inContains);
+      // const inContains = judgeContains(IconsRef, ModalFrameRef);
+      // // 存储状态
+      // deskTopStore.changeCurrentSelected(inContains);
     };
     document.addEventListener('mousemove', move);
     document.addEventListener('mouseup', () => {
